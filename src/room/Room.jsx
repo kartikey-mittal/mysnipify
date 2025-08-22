a268bedd59112369b54222423f954920e3054772
Kartikey Mittal <41264448+kartikey-mittal@users.noreply.github.com>
2025-08-23 01:39:52 +0530

    Update Room.jsx

--- src/room/Room.jsx
+++ src/room/Room.jsx
@@ -12,8 +12,8 @@ const Room = () => {
 
     if (skilledName || learnerName) {
       const userName = skilledName || learnerName;
-      const appID = 1398404057;
-      const serverSecret = "bc80abde3c8eb871254d42beadc9eff1";
+      const appID = 079135;
+      const serverSecret = "62ace8f43ba236fa835d1528bbbe5222";
       const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), userName);
 
       const zp = ZegoUIKitPrebuilt.create(kitToken);
